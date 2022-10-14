a753faaa3a97397a3d5d5a8ac4c0b9e40a330583
Олександр <102046268+AlexElektrik@users.noreply.github.com>
2022-10-14 14:13:13 +0300

    виконав 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBt = document.querySelector('[data-start]');
+const stopBt = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let updateColorId = null;
 
+startBt.addEventListener("click", onStartBtClick);    
+stopBt.addEventListener("click", onStopBtClick);
+
+function onStartBtClick(e) {
+   if (!updateColorId ) {updateColorId = setInterval(() =>
+        body.style.backgroundColor = `${getRandomHexColor()}`, 1000); 
+       console.log('Start');
+    };
+};
+
+function onStopBtClick(e) {
+    console.log('stop');
+    clearInterval(updateColorId);
+    updateColorId = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
